32a6ca327d2f4a6301a3be90bfe5dbbef7c9844e
Mohammad Hatif Osmani <66199079+hatif03@users.noreply.github.com>
2025-01-20 04:52:50 +0530

    building the contract

---
+++ src/SimplePredictionMarket.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {IERC20} from "@thirdweb-dev/contracts/eip/interface/IERC20.sol";
+import {Ownable} from "@thirdweb-dev/contracts/extension/Ownable.sol";
+import {ReentrancyGuard} from "@thirdweb-dev/contracts/external-deps/openzeppelin/security/ReentrancyGuard.sol";
+
+contract SimplePredictionMarket is Ownable, ReentrancyGuard {
+    enum MarketOutcome {
+        UNRESOLVED,
+        OPTION_A,
+        OPTION_B
+    }
+
+    struct Market {
+        string question;
+        uint256 endTime;
+        MarketOutcome outcome;
+        string optionA;
+        string optionB;
+        uint256 totalOptionAShares;
+        uint256 totalOptionBShares;
+        bool resolved;
+        mapping(address => uint256) optionASharesBalance;
+        mapping(address => uint256) optionBSharesBalance;
+    }
+
+    IERC20 public bettingToken;
+    uint256 public marketCount;
+    mapping(uint256 => Market) public markets;
+
+    event MarketCreated(
+        uint256 indexed marketId,
+        string question,
+        string optionA,
+        string optionB,
+        uint256 endTime
+    );
+
+    event SharesPurchased(
+        uint256 indexed marketId,
+        address indexed buyer,
+        bool isOptionA,
+        uint256 amount
+    );
+
+    event MarketResolved(uint256 indexed marketId, MarketOutcome outcome);
+
+    event Claimed(
+        uint256 indexed marketId,
+        address indexed user,
+        uint256 amount
+    );
+
+    constructor(address _bettingToken) {
+        bettingToken = IERC20(_bettingToken);
+        _setupOwner(msg.sender); // set the contract deployer as the owner
+    }
+}
